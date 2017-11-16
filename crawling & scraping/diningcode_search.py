b841cbb3d53626a01ac3c51a301de1a115cb6fac
kevin <kevin.lee@mangoplate.com>
2017-11-16 20:09:50 +0900

    소스 정리

--- crawling & scraping/diningcode_search.py
+++ crawling & scraping/diningcode_search.py
@@ -3,22 +3,25 @@ from selenium import webdriver
 print("## 검색어를 입력하시오. ")
 keyword = input()
 
-# 드라이버 추출하기 
+# 드라이버 추출하기
 driver = webdriver.Chrome()
 
-# 검색 페이지 로딩 
+# 검색 페이지 로딩 ( 3초 기다림 )
 path = "https://www.diningcode.com/isearch.php?query=" + keyword
 driver.get(path)
 print("## 페이지 로딩", path, sep=' : ')
 driver.implicitly_wait(3)
 
-# 추천 카테고리로 가져오기 
+# 추천 카테고리로 가져오기
+# //*[@id="main_area"]/div[1]/div[3]/div/div/div[1]/a[4]
 tags = driver.find_elements_by_xpath("//*[@id='main_area']/div/div/div/div/div/a")
 print("## '", keyword, "' 추천 카테고리")
 for tag in tags :
-    print("\t - ", tag.text)
+    if tag.text :
+        print("\t - ", tag.text)
 
 print("## 추천 레스토랑 ")
+# //*[@id="main_area"]/div[3]/div[3]/div[1]/div[3]/div[2]/div[1]/a
 resturant_names = driver.find_elements_by_xpath("//*[@id='main_area']/div/div/div/div/div/div/a")
 for name in resturant_names :
     print("\t - ", name.text)
--- crawling & scraping/execute_javascript.py
+++ crawling & scraping/execute_javascript.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 
 browser = webdriver.Chrome()
 browser.get("http://en.wikipedia.org")
 
 browser.execute_script("alert('Hello, World!')")
+
+
